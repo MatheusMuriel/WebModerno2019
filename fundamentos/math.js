7c1a9aaa99b76ef81b35472c58de0d6212a5e228
Muriel <matheus.muriel@outlook.com>
2019-12-17 17:32:54 -0300

    Math

---
+++ fundamentos/math.js
@@ -0,0 +1,5 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+console.log(area)
+console.log(typeof Math) // Math não é uma função... Ele é um objeto que tem constantes, como o "PI", e funções como o "pow"
